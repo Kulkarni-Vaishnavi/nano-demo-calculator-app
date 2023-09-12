5ea1c2b23cf7d975a841f89064c86508b8b50c54
Kulkarni-Vaishnavi <srivaishnavi2605@gmail.com>
2023-09-12 17:18:01 +0530

    3rd commit

--- server.js
+++ server.js
@@ -10,23 +10,18 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    res.status(200);
-    return res.send('Hello World');
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    const first = req.body.first;
-    const second = req.body.second;
-    const ans = first+second;
-    res.json({ "result": ans});
+    const {first, second} = req.body;
+    res.json({ "result": first + second});
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    const first = req.body.first;
-    const second = req.body.second;
-    const ans = first - second;
-    res.json({ "result": ans });
+    const {first, second} = req.body;
+    res.json({ "result": first  - second});
 });
 
 app.use(baseUrl, baseRouter);
